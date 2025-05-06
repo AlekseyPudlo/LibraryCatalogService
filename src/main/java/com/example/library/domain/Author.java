e7843d688e9a7923fe61971cd0b242c1845ab71b
Oleksii Pudlo <pudloaleksey@gmail.com>
2025-05-06 14:40:21 +0200

    feat: update Author model with validation and relationships; add BookTest for unit testing

--- src/main/java/com/example/library/domain/Author.java
+++ src/main/java/com/example/library/domain/Author.java
@@ -1,7 +1,33 @@
 package com.example.library.domain;
 
+import java.util.Set;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "authors")
 public class Author {
-  private String name;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String firstName;
+
+  @NotBlank
+  @Column(nullable = false)
+  private String lastName;
+
+  @Email
   private String email;
+
+  @Size(max = 1000)
   private String bio;
+
+  @OneToMany(mappedBy = "author")
+  private Set<Book> books;
+
+  // Constructor, getters, setters
 }
